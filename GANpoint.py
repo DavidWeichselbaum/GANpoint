8ef0d9991b38c91120d2ef939bbb2cbbef1bf510
David T. Weichselbaum <david.t.weichselbaum@gmail.com>
2019-04-23 21:02:17 +0200

    Add y sampling

--- GANpoint.py
+++ GANpoint.py
@@ -5,27 +5,18 @@ import tensorflow as tf
 import tensorflow_hub as hub
 from tqdm import tqdm
 
+batch_size = 1
+truncation = 0.5  # noise truncation value
 
 os.environ["TFHUB_CACHE_DIR"] = './models'
 module = hub.Module('https://tfhub.dev/deepmind/biggan-deep-512/1')
 
-batch_size = 1
-truncation = 0.5  # scalar truncation value in [0.0, 1.0]
-
-z_np = truncation * np.random.normal(size=(batch_size, 128)).astype('f')
-z = tf.Variable(z_np)
-
-# y_index = tf.random.uniform([batch_size], maxval=1000, dtype=tf.int32)
-# y = tf.one_hot(y_index, 1000)  # one-hot ImageNet label
-
-y_np = np.zeros((batch_size, 1000)).astype('f')
-y_np[0, 0] = 1.0
-y = tf.Variable(y_np)
+z = tf.Variable(np.zeros((batch_size, 128)).astype('f'))
+y = tf.Variable(np.zeros((batch_size, 1000)).astype('f'))
 
 # Call BigGAN on a dict of the inputs to generate a batch of images with shape
 # [1, 512, 512, 3] and range [-1, 1].
 samples = module(dict(y=y, z=z, truncation=truncation))
-
 
 sess = tf.Session()
 with sess.as_default():
@@ -36,7 +27,10 @@ with sess.as_default():
     while True:
         z_np = truncation * np.random.normal(size=(batch_size, 128)).astype('f')
 
-        image = sess.run(samples, feed_dict={z: z_np})
+        y_np = np.random.uniform(size=(batch_size, 1000)).astype('f')
+        y_np = y_np / y_np.sum()  # sum of probabilities should be 1.0
+
+        image = sess.run(samples, feed_dict={z: z_np, y: y_np})
 
         image = np.squeeze(image)
         image = (image + 1) / 2
@@ -53,5 +47,5 @@ with sess.as_default():
             while cv2.waitKey(100) != 32:  # spacebar: pause on image
                 pass
 
-        cv2.imshow('Visions', image)
+        cv2.imshow('Visions at GANpoint', image)
         progress.update()
